ec2e0c82397e27e95047c6144bdc934559145047
BIT504 <cassandrasampson28@gmail.com>
2020-10-28 08:03:24 -0500

    With Sprite class

--- pingPong/src/pingPong/PongPanel.java
+++ pingPong/src/pingPong/PongPanel.java
@@ -5,16 +5,36 @@
    import java.awt.event.KeyEvent;
    import java.awt.event.KeyListener;
    import javax.swing.JPanel;
+   import javax.swing.Timer;
+   import java.awt.Graphics;
+   import java.awt.Graphics2D;
+   import java.awt.Stroke;
+   import java.awt.BasicStroke;
+   import java.awt.Graphics2D;
+
    
    
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
        
 		private final static Color BACKGROUND_COLOR = Color.BLACK;
+		private final static int TIMER_DELAY = 5;
 
       public PongPanel() {
           setBackground(BACKGROUND_COLOR);
+          Timer timer = new Timer(TIMER_DELAY, this);
+          	timer.start();
       }
-  
+      
+      private void update() {
+    	  
+      }        
+      
+      @Override
+      public void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          paintDottedLine(g);
+      }
+      
       @Override
       public void keyPressed(KeyEvent event) {
       }
@@ -29,6 +49,17 @@
   
       @Override
       public void actionPerformed(ActionEvent event) {
+    	     update();
+    	     repaint();
       }
+      
+      private void paintDottedLine(Graphics g) {
+          Graphics2D g2d = (Graphics2D) g.create();
+             Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+             g2d.setStroke(dashed);
+             g2d.setPaint(Color.WHITE);
+             g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+             g2d.dispose();
+     }
   
  }
